872888731c33d5ab18f8809190eb92472229c4da
paulkiwana <paulkiwana6@gmail.com>
2024-07-28 09:58:12 +0300

    working on the hero section still

--- scripts.js
+++ scripts.js
@@ -172,3 +172,23 @@ textureImg.addEventListener('click', ()=>{
 texture.style.display = 'flex';
 navImg.style.display = 'none';
 })
+
+
+
+//slideshow logic
+let slideshow = 0;
+showSlides()
+
+function showSlides(){
+let slides = document.getElementByClassName("mySlides");
+for(let i = 0; i < slides.length; i++){
+slides[i].style.display = 'none'
+ }
+slideIndex++;
+if(slideIndex > slides.length){
+slideIndex = 1;
+}
+slides[slideIndex-1].style.display = "block"
+setTimeout(showSlides,5000);
+}
+}
