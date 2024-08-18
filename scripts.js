e7e5b59aedd7ada25e8db0a2ba7618b13dd0c113
Nsibirwa Paul Kiwana <paulkiwana6@gmail.com>
2024-08-18 15:51:33 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -31,16 +31,16 @@ contactForm.style.display = contactForm.style.display === 'block'? 'none' : 'non
 interior.style.display = interior.style.display === 'block'? 'none':'none';
 texture.style.display = texture.style.display === 'block'? 'none':'none';
 exterior.style.display = exterior.style.display === 'block'? 'none':'none';
-navImg.style.display = window.innerWidth >= 800 ? 'grid' : 'block';
+const updateDisplay = ()=> {
+    navImg.style.display = window.innerWidth >= 800 ? 'grid' : 'block';
+}
     formMessage.textContent = '';
     welcome.style.display = "block";
 !toggleSidebar()
-
+updateDisplay()
 })
 //logic to listen for the resize event so that we don't get the wrong display in the wrong view port width
-window.addEventListener('resize', ()=>{
-    navImg.style.display = window.innerWidth >= 800 ? 'grid' : 'block';
-    });
+window.addEventListener('resize', updateDisplay);
 
 
 // logic for about-us toggle
